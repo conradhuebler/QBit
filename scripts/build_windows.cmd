f677d2d9fe30a3bc892e470550935c005d80765e
contra98 <Conrad.Huebler@student.tu-freiberg.de>
2017-11-11 17:31:09 +0100

    more changes

--- scripts/build_windows.cmd
+++ scripts/build_windows.cmd
@@ -2,23 +2,19 @@ echo on
 
 SET project_dir="%cd%"
 
-echo Set up environment...
-set PATH=%QT%\bin\;C:\Qt\Tools\QtCreator\bin\;C:\Qt\QtIFW2.0.1\bin\;%PATH%
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" %PLATFORM%
-
-
 echo Building QBit...
 
 mkdir build
 cd build
-cmake .. "-DCMAKE_BUILD_TYPE=Release" "-DCMAKE_PREFIX_PATH=%QT%"
-nmake
+cmake .. -G %makefiles% -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="c:/dev/install"
+if not %platform%==mingw (nmake) else (mingw32-make)
+if not %platform%==mingw (nmake install) else (mingw32-make install)
 
 echo Running tests...
 
 echo Packaging...
 cd %project_dir%\build\windows\msvc\x86_64\release\
-windeployqt --qmldir ..\..\..\..\..\src\ QBit.exe
+windeployqt QBit.exe
 
 echo Copying project files for archival...
 copy "%project_dir%\README.md" "QBit\README.md"
