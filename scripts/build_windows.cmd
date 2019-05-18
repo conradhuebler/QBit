b4d298fceb7eb85252cab97bfd398f5b83949d25
Conrad Huebler <Conrad.Huebler@chemie.tu-freiberg.de>
2019-05-18 12:25:13 +0200

    dir struct test

--- scripts/build_windows.cmd
+++ scripts/build_windows.cmd
@@ -17,7 +17,6 @@ mkdir QBit
 copy QBit.exe QBit
 cd QBit
 windeployqt --release QBit.exe
-copy C:\Qt\Tools\mingw530_32\bin\libgomp-1.dll  "%project_dir%\build\QBit\libgomp-1.dll
 
 echo Copying project files for archival...
 copy "%project_dir%\README.md" "%project_dir%\build\QBit\README.md"
@@ -25,4 +24,5 @@ copy "%project_dir%\LICENSE.md" "%project_dir%\build\QBit\LICENSE.md"
 
 echo Packaging portable archive...
 cd ..
-7z a QBit_nightly_%TAG_NAME%_windows.zip QBit
+cd ..
+7z a QBit_nightly_%TAG_NAME%_windows.zip build
