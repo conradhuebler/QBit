00fac5ac7c81b4d820d452c51beabc351845cabc
Conrad Hübler <Conrad.Huebler@chemie.tu-freiberg.de>
2019-11-25 22:48:03 +0100

    more tests

--- scripts/build_unix.sh
+++ scripts/build_unix.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -ex
-
+cd external/libpeakpick
+git submodule init
+git submodule update --recursive
+cd ..
+cd ..
 if [ "$CXX" = "g++" ]; then export CXX="g++-6" CC="gcc-6"; fi # else export CXX="clang++" CC="clang"; fi
 mkdir -p build
 cd build
