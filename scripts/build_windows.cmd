21a5173960cd55a3d9f96975d64be49b5ffd4ddb
Conrad Hübler <Conrad.Huebler@chemie.tu-freiberg.de>
2019-11-26 20:52:03 +0100

    test

--- scripts/build_windows.cmd
+++ scripts/build_windows.cmd
@@ -2,26 +2,25 @@ echo on
 
 SET project_dir="%cd%"
 
-echo Building QBit...
+echo Building SupraFit...
 git submodule update --init --recursive
 git pull --recurse-submodules
-mkdir build
-cd build
-cmake -G "Visual Studio 15 2017 Win64" -DCMAKE_BUILD_TYPE=Release ..
+mkdir build_x64
+cd build_x64
+cmake -G "Visual Studio 16 2019 Win64" -DCMAKE_BUILD_TYPE=Release
 cmake --build . --config Release
 
 
 echo Packaging...
-cd %project_dir%\build\Release
+cd %project_dir%
+cd build_x64\bin\win32\Release
 mkdir QBit
-copy QBit.exe QBit
+copy suprafit.exe QBit
+copy suprafit_cli.exe QBit
+
 cd QBit
 windeployqt --release QBit.exe
 
-echo Copying project files for archival...
-copy "%project_dir%\README.md" "%project_dir%\build\Release\QBit\README.md"
-copy "%project_dir%\LICENSE.md" "%project_dir%\build\Release\QBit\LICENSE.md"
-
 echo Packaging portable archive...
 cd ..
-7z a QBit_nightly_%TAG_NAME%_windows.zip QBit
+7z a QBit_latest_x64_windows.zip QBit
