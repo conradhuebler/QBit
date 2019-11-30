35e3c2a970b5a2366540532b2984f488cb7187ac
Conrad Hübler <Conrad.Huebler@chemie.tu-freiberg.de>
2019-11-30 15:24:18 +0100

    remove travis and appvoyer

--- scripts/build_windows.bat
+++ scripts/build_windows.bat
@@ -2,12 +2,12 @@ echo on
 
 SET project_dir="%cd%"
 
-echo Building SupraFit...
+echo Building QBit x32 and x64...
 git submodule update --init --recursive
 git pull --recurse-submodules
 mkdir build_x64
 cd build_x64
-cmake -G "Visual Studio 16 2019" -DCMAKE_BUILD_TYPE=Release ..
+cmake -G "Visual Studio 15 2017 Win64" -DCMAKE_BUILD_TYPE=Release ..
 cmake --build . --config Release
 
 
@@ -22,3 +22,23 @@ windeployqt --release QBit.exe
 echo Packaging portable archive...
 cd ..
 7z a QBit_latest_x64_windows.zip QBit
+
+cd ..
+
+mkdir build_x32
+cd build_x32
+cmake -G "Visual Studio 15 2017 Win32" -DCMAKE_BUILD_TYPE=Release ..
+cmake --build . --config Release
+
+
+echo Packaging...
+cd %project_dir%
+cd build_x32\Release
+mkdir QBit
+copy QBit.exe QBit
+cd QBit
+windeployqt --release QBit.exe
+
+echo Packaging portable archive...
+cd ..
+7z a QBit_latest_x32_windows.zip QBit
